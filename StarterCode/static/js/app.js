ca388ac4654cb9d09f46a120e8b0dc6b06466bae
Adam Johnson <adam.johnson57@gmail.com>
2021-01-09 18:28:49 -0600

    progressing on app

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,15 +1,25 @@
 // from data.js
 var tableData = data;
 
-//select the filter button
+// Select the filter button
 var filter_button = d3.select("#filter-btn");
 
-//select the form
+// Select the form
 var form = d3.select("form");
 
 // Reference to the table body
 var tbody = d3.select("tbody");
 
 
+// Append data.js to table body
+tableData.forEach(function(ufoSighting) {
+    console.log(ufoSighting);
+    var row = tbody.append("tr");
+    Object.entries(ufoSighting).forEach(function([key, value]) {
+        console.log(key, value);
+        var cell = row.append("td");
+        cell.text(value);     
+    });
+});
 
 
